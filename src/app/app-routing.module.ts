7a19810572f0f2c97432039bce237980d3261ef3
h.alnuwaysir <H.alnuwaysir@tamkeentech.sa>
2023-10-17 09:01:32 +0300

    add officee work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component:DashboardComponent },
+  {path: 'login', component:LoginComponent },
+  {path: 'register', component:RegisterComponent },
+  {path: 'client/add', component: AddClientComponent},
+  {path: 'client/edit/:id', component: EditClientComponent},
+  {path: 'client/:id', component: ClientDetailsComponent},
+  {path: 'settings', component: SettingsComponent},
+  {path: '**', component:NotFoundComponent }
+  // {path: '', component: },
+  // {path: '', component: },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyArgvuIYE5Z4IWUGWfPMzRe0BqrNnvrsrg",
+    authDomain: "clientpanelprod-73115.firebaseapp.com",
+    projectId: "clientpanelprod-73115",
+    storageBucket: "clientpanelprod-73115.appspot.com",
+    messagingSenderId: "1043464774343",
+    appId: "1:1043464774343:web:7d152654dbf66b7e7714e2",
+    measurementId: "G-1K8MSJSV2N",
+
+  }
+};
